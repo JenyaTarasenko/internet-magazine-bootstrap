dca571a430884659b9f44d8d3ba7c9d7da7b6a67
Kiril <kiril@Mac-jenya.local>
2024-01-28 18:56:20 +0200

    card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+// console.log('hello');
+
+
+window.addEventListener('scroll', function(){
+    document.getElementById('header-nav').classList.toggle('headernav-scrol', window.scrollY > 135);
+
+});
